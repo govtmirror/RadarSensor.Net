8d3ab30ed5492ac882fed7262f1650dc6d8b78cb
Earl Dean <edean@its.bldrdoc.gov>
2016-10-31 17:31:10 -0600

    Added RaderSenserProcess project to git

---
+++ RadarSensorProcess/RadarSensorProcess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AgilentN6841A;
+using Preselctor;
+using Logging;
+
+namespace RadarSensorService
+{
+    internal class RadarSensorService
+    {
+        [STAThread]
+        public static void Main(String[] args)
+        {
+            Logger.logMessage("Starting Radar Sensor process");
+
+            while(true)
+            {
+
+            }
+        }
+
+    }
+}
